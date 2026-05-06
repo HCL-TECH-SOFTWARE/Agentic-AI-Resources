3e40f3dcd2c58ff8d491ae1263aa4a26a54d370b
Elavarasan Soundararajan <elavarasan.sound@prod.hclpnp.com>
2026-05-06 09:55:28 +0530

    License updated commit.

--- sql/budgetoptimization.sql
+++ sql/budgetoptimization.sql
@@ -1,3 +1,17 @@
+﻿-- Copyright 2026 HCL America, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- This SQLite script creates a personal budget and expense table.
 -- It is populated with 50 realistic records, including recurring and one-time expenses.
 -- The data for May 2025 is designed to exceed a simulated monthly income of INR 1,20,000
--- sql/financial_advisorr.sql
+++ sql/financial_advisorr.sql
@@ -1,3 +1,17 @@
+﻿-- Copyright 2026 HCL America, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- This SQLite script creates and populates three tables based on the provided finance PDF documents:
 -- 1. AssetAllocationGuidelines: Derived from finance1.pdf
 -- 2. DebtAndLiquidityGuidelines: Derived from finance2.pdf
